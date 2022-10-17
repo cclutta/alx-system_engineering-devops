25996ea3c115cb0ec330a0959537383dc9b4310c
cclutta <52520502+cclutta@users.noreply.github.com>
2022-10-17 22:44:49 +0300

    Create 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Python script that exports data to CSV
+"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
+
+    with open('{}.csv'.format(id), 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for t in todos:
+            row = [id, user.get("username"), t.get("completed"), t.get("title")]
+            row = [str(value) for value in row]
+            csv_writer.writerow(row)
