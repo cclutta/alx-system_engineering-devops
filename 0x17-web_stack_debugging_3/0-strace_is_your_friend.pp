f77843dbbbd39fd4faf70178a2e1b10ea7715524
cclutta <chelsea.lutta@gmail.com>
2022-11-02 18:10:13 +0300

    Update

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Web-stack debugging script in puppet
+exec { 'wsd3':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    =>  ['/bin', '/usr/bin']
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,4 @@
+# 0x17. Web stacj debugging #3
+## General
+- Web server
+- Web stack debugging
